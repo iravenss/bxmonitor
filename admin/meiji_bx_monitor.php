14f4bb758cf49dd50b7f79336682d874fd0644a7
meiji <voronov@meiji.media>
2020-09-22 19:39:47 +0300

    Доработки

--- admin/meiji_bx_monitor.php
+++ admin/meiji_bx_monitor.php
@@ -8,31 +8,73 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_ad
 Loc::loadMessages(__FILE__);
 
 
-
 $APPLICATION->SetTitle(Loc::getMessage('MEIJI_SOLUTION_MONITOR_ADMIN_PAGE_TITLE'));
 
+$allData = \Meiji\BxMonitor\Manager::getVisibleData();
 
+foreach ($allData as $arSite) {
+    $tabs[] = [
+        'DIV' => 'monitor' . $arSite['SITE']['LID'],
+        'TAB' => Loc::getMessage('MEIJI_SOLUTION_MONITOR_TAB1_NAME') . $arSite['SITE']['LID'],
+        'TITLE' => Loc::getMessage('MEIJI_SOLUTION_MONITOR_TAB1_TITLE') . $arSite['SITE']['LID'],
+    ];
 
-$tabControl = new CAdminTabControl('tabControl', [
-	[
-		'DIV'   => 'edit' . $arSite['LID'],
-		'TAB'   => Loc::getMessage('MEIJI_SOLUTION_SETTINGS_ADMIN_PAGE_TAB1_NAME'),
-		'TITLE' => Loc::getMessage('MEIJI_SOLUTION_SETTINGS_ADMIN_PAGE_TAB1_TITLE')
-	]
-], false, false);
+}
+
+$tabControl = new CAdminTabControl('tabControl', $tabs, false, false);
+
 
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_after.php');
 ?>
 
 <?
 $tabControl->Begin();
-$tabControl->BeginNextTab();
+
+foreach ($allData as $arVisibleData):
+    $tabControl->BeginNextTab();
 
 
-?>
+    ?>
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>Имя</td>
+            <td>Тип</td>
+            <td>Элементов</td>
+            <td>Разделов</td>
+        </tr>
+        <? foreach ($arVisibleData['IBLOCKS'] as $arIblock): ?>
+            <tr>
+                <td><?= $arIblock['ID'] ?></td>
+                <td><?= $arIblock['NAME'] ?></td>
+                <td><?= $arIblock['IBLOCK_TYPE_ID'] ?></td>
+                <td><?= $arIblock['ELEMENTS_CNT'] ?></td>
+                <td><?= $arIblock['SECTIONS_CNT'] ?></td>
+            </tr>
+        <? endforeach; ?>
 
+    </table>
 
-    <?
+    <table>
+        <tr>
+            <td></td>
+            <td>Типов инфоблоков</td>
+            <td>Инфоблоков</td>
+            <td>Разделов</td>
+            <td>Элементов</td>
+        </tr>
+        <tr>
+            <td><b>Итого</b></td>
+            <td><?= $arVisibleData['ALL']['TYPES'] ?></td>
+            <td><?= $arVisibleData['ALL']['IBLOCKS'] ?></td>
+            <td><?= $arVisibleData['ALL']['SECTIONS'] ?></td>
+            <td><?= $arVisibleData['ALL']['ELEMENTS'] ?></td>
+        </tr>
+
+    </table>
+<? endforeach; ?>
+<?
+
 $tabControl->End();
 ?>
 
--- src/Manager.php
+++ src/Manager.php
@@ -16,7 +16,124 @@ use Bitrix\Main\Loader;
 class Manager extends \Meiji\BxMarketplace\Fixtures\AbstractSolution
 {
 
-    public function someFunc(){
+
+    public function getVisibleData()
+    {
+        \CModule::IncludeModule("iblock");
+
+        $arFullData = [];
+
+        $arSites = self::getAllSites();
+
+        foreach ($arSites as $arSite) {
+            $siteData = self::collectDataAboutSite($arSite['ID']);
+            $siteData['SITE'] = $arSite;
+            $arFullData[] = $siteData;
+        }
+        return $arFullData;
+
+    }
+
+    /**
+     * @return array
+     */
+    public static function getAllSites()
+    {
+        $rsSites = \CSite::GetList($by = "sort", $order = "desc", Array());
+        while ($site = $rsSites->Fetch()) {
+            $sites[] = $site;
+        }
+
+        return $sites;
+    }
+
+    /**
+     * @param $siteId
+     * @return mixed
+     */
+    public static function collectDataAboutSite($siteId)
+    {
+        $arData['IBLOCKS'] = self::getIblocks($siteId);
+        $arData = self::countIblockElements($arData);
+        $arData = self::countIblockSections($arData);
+        $arData = self::countAll($arData);
+        return $arData;
+    }
+
+    /**
+     * @param $siteId
+     * @return array
+     */
+    public static function getIblocks($siteId)
+    {
+
+        $res = \CIBlock::GetList(
+            Array(),
+            Array(
+                'SITE_ID' => $siteId
+            ), true
+        );
+        while ($ar_res = $res->Fetch()) {
+            $forRet[] = $ar_res;
+        }
+        return $forRet;
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public static function countIblockElements($data)
+    {
+        foreach ($data['IBLOCKS'] as &$arIblock) {
+            $cnt = \CIBlockElement::GetList(
+                array(),
+                array('IBLOCK_ID' => $arIblock['ID']),
+                array(),
+                false,
+                array('ID', 'NAME')
+            );
+            $arIblock['ELEMENTS_CNT'] = $cnt;
+
+        }
+        return $data;
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public static function countIblockSections($data)
+    {
+        foreach ($data['IBLOCKS'] as &$arIblock) {
+
+            $arFilter = array(
+                'IBLOCK_ID' => $arIblock['ID'], // ID инфоблока
+            );
+            $arIblock['SECTIONS_CNT'] = \CIBlockSection::GetCount($arFilter);
+
+
+        }
+        return $data;
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public static function countAll($data)
+    {
+
+        foreach ($data['IBLOCKS'] as $arIblock) {
+            $countAll['ALL_TYPES'][] = $arIblock['IBLOCK_TYPE_ID'];
+            $countAll['IBLOCKS']++;
+            $countAll['ELEMENTS'] += $arIblock['ELEMENTS_CNT'];
+            $countAll['SECTIONS'] += $arIblock['SECTIONS_CNT'];
+
+        }
+        $data['ALL'] = $countAll;
+        $data['ALL']['TYPES'] = count(array_unique($countAll['ALL_TYPES']));
+        return $data;
 
     }
 
